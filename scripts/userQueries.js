ce9e01d63ad5c95fe20ea3bb410a1039e2c46205
sadigovafarida <farida.sadigova@stackerine.com>
2018-10-16 17:35:56 +0200

    scripts

---
+++ scripts/userQueries.js
@@ -0,0 +1,18 @@
+/* I WANNA KNOW EVERYTHING ABOUT YOU */
+
+const userName = prompt('Fill your name:');
+const userFirstName = prompt('Fill your Firstname:');
+const userAge = prompt('Fill your age:');
+
+console.log(
+  'Bonjour ' +
+    userFirstName +
+    ' ' +
+    userName +
+    ' ' +
+    ' . ' +
+    'Vous avez ' +
+    userAge +
+    ' ' +
+    ' ans '
+);
